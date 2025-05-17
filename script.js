25f9514647cfeb9e6fa8666dd507addefe2b1379
UnknownSkill-web <noflo.keilwerth@gmail.com>
2025-05-17 03:22:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
 particlesJS('particles-js', {
-    "particles": {
-      "number": {
-        "value": 100
+    particles: {
+      number: {
+        value: 100
       },
-      "color": {
-        "value": "#ffffff"
+      color: {
+        value: "#ffffff"
       },
-      "shape": {
-        "type": "circle"
+      shape: {
+        type: "circle"
       },
-      "opacity": {
-        "value": 0.5
+      opacity: {
+        value: 0.5
       },
-      "size": {
-        "value": 2
+      size: {
+        value: 2
       },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1
       },
-      "move": {
-        "enable": true,
-        "speed": 1.5
+      move: {
+        enable: true,
+        speed: 1.5
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
+    interactivity: {
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse"
+        }
+      },
+      modes: {
+        repulse: {
+          distance: 60,  // 👈 kleinerer Radius
+          duration: 0.4
         }
       }
     },
-    "retina_detect": true
+    retina_detect: true
   });
   
