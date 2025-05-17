7d015fe1b43c5d7556692ba7bcc4f4ca60ab7d6e
UnknownSkill-web <noflo.keilwerth@gmail.com>
2025-05-17 03:08:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": 100
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle"
+      },
+      "opacity": {
+        "value": 0.5
+      },
+      "size": {
+        "value": 2
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.5
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
